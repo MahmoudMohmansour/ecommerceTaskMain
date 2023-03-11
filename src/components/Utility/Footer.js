875d2e2fd63e6d546a59fbfefc6e4283bfb9e0cc
MahmoudMohmansour <mansmahmoud141@gmail.com>
2023-03-11 13:15:02 +0200

    fix responsive

--- src/components/Utility/Footer.js
+++ src/components/Utility/Footer.js
@@ -4,6 +4,10 @@ import facebook from "../../images/facebook.png";
 import instagram from "../../images/instagram.png";
 import twitter from "../../images/twitter.png";
 import phone from "../../images/phone.png";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser ,faCartShopping, faPhone,   } from '@fortawesome/free-solid-svg-icons'
+import { faFacebookF, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons'
+
 const Footer = () => {
     return (
         <div className="footer-background footer mt-3 pt-2" style={{ maxHeight: "50px" }}>
@@ -18,17 +22,17 @@ const Footer = () => {
                         sm="6"
                         className="d-flex justify-content-end align-items-center footer-end ">
                         <div className="d-flex pt-3 mx-2">
-                            <img width="20px" height="20px" src={phone} alt="" />
+                        <FontAwesomeIcon className="footer-icon" icon={ faPhone}/>
                             <p className="footer-phone">01018949514</p>
                         </div>
                         <div style={{ cursor: "pointer" }}>
-                            <img width="20px" height="20px" src={facebook} alt="" />
+                        <FontAwesomeIcon className="footer-icon" icon={ faFacebookF}/>
                         </div>
                         <div style={{ cursor: "pointer" }} className="">
-                            <img width="20px" height="20px" src={instagram} alt="" />
+                        <FontAwesomeIcon className="footer-icon" icon={ faTwitter}/>
                         </div>
                         <div style={{ cursor: "pointer" }} className="">
-                            <img width="20px" height="20px" src={twitter} alt="" />
+                        <FontAwesomeIcon className="footer-icon" icon={ faInstagram}/>
                         </div>
                     </Col>
                 </Row>
